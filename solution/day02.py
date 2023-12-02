e109df300276cef3130f3c13c792a5a07ba5c7f1
Jan Travnicek <jan.travnicek@bloomreach.com>
2023-12-02 22:39:34 +0100

    day02 complete

--- solution/day02.py
+++ solution/day02.py
@@ -1,17 +1,31 @@
 import re
 
-def __is_possible(moves, game_cubes) -> bool:
-    for move in moves:
-        cubes = re.findall(r"(\d+) (\w+)", move)
-        for cube in cubes:
-            if int(cube[0]) > game_cubes[cube[1]]:
-                return False
+def __is_possible(game, game_cubes) -> bool:
+    cubes = re.findall(r"(\d+) (\w+)", game)
+    for cube in cubes:
+        if int(cube[0]) > game_cubes[cube[1]]:
+            return False
     return True
 
 def sum_possible_games(games, game_cubes):
     sum = 0
-    for i, line in enumerate(games, start=1):
-        moves = line.split(':')[1].split(';')
-        if __is_possible(moves, game_cubes):
+    for i, game in enumerate(games, start=1):
+        game = game.split(':')[1]
+        if __is_possible(game, game_cubes):
             sum += i
+    return sum
+
+def __get_power(game):
+    power = { 'red': 0, 'green': 0, 'blue': 0 }
+    cubes = re.findall(r"(\d+) (\w+)", game)
+    for cube in cubes:
+        number_of_cubes = int(cube[0])
+        color_of_cubes = cube[1]
+        power[color_of_cubes] = max(number_of_cubes, power[color_of_cubes])
+    return power['red'] * power['green'] * power['blue']
+
+def get_sum_of_the_powers(games):
+    sum = 0
+    for game in games:
+        sum += __get_power(game)
     return sum
--- tests/test_day02.py
+++ tests/test_day02.py
@@ -1,5 +1,5 @@
 from solution.common import get_lines_from_file
-from solution.day02 import sum_possible_games
+from solution.day02 import sum_possible_games, get_sum_of_the_powers
 
 first_part_cubes = {
         'red':   12,
@@ -14,3 +14,11 @@ def test_first_part_example():
 def test_first_part_solution():
     lines = get_lines_from_file('./data/day02/input.data')
     assert sum_possible_games(lines, first_part_cubes) == 2278
+
+def test_second_part_example():
+    lines = get_lines_from_file('./data/day02/example01.data')
+    assert get_sum_of_the_powers(lines) == 2286
+
+def test_second_part_solution():
+    lines = get_lines_from_file('./data/day02/input.data')
+    assert get_sum_of_the_powers(lines) == 67953
